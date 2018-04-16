d12282ecd1b5fee154cf90b2f4f68adb9c5bb13b
gloria <pureainu@gmail.com>
2018-04-17 00:09:28 +0900

    add router sample

--- backend/src/main/java/gloria/controller/RouteController.java
+++ backend/src/main/java/gloria/controller/RouteController.java
@@ -18,4 +18,11 @@ public class RouteController {
     public String redirect() {
         return "forward:/";
     }
+
+    // 만약, 각 페이지에 대해 라우터 설정을 하고자 하는 경우
+//    @RequestMapping(path = "/doc/**")
+//    public String demo(HttpServletRequest request) {
+//        String path = request.getRequestURI().replaceAll("^/", "");
+//        return path + "/index";
+//    }
 }
